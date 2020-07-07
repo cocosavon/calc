7661245f332c43dfbeac0870efc946ef26ea040c
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-07 17:56:10 +0800

    Add json data

--- calc.js
+++ calc.js
@@ -1,7 +1,14 @@
+import MyData from './my_data.json'
 new Vue({
     el: "#vue_component",
     data: {
         options_oil: ['olive', 'canora',],
         selection: null,
-    }
+    },
+    created: function(){
+        console.log('in created')
+    },
+    mounted: function(){
+        console.log('in mounted')
+    },
 })
