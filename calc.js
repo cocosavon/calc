40c3bfa614836f5f397aa9173de6da79f4ad3cfc
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-08 08:48:35 +0800

    Introduce the firestore as a database

--- calc.js
+++ calc.js
@@ -7,16 +7,32 @@ new Vue({
     created: function(){
         console.log('in created')
     },
+    methods: {
+        saveToFirebase: function(name) {
+            console.log('in saveToFirebase')
+            var obj = {
+                'name': name
+            };
+        }
+    },
     mounted: function(){
         console.log('in mounted')
-        
         let vm = this
 
-        axios.get("https://cocosavon.github.io/calc/my_data.json")
-            .then(response => {
-                console.log(response)
+        //axios.get("https://cocosavon.github.io/calc/my_data.json")
+        //    .then(response => {
+        //        vm.options_oil = response.oils
+        //    })
 
-                vm.options_oil = response.oils
-            })
+        let docRef = db.collection("oils")
+        console.log(docRef)
+        docRef.get().then(function(res) {
+            res.forEach(doc => {
+                console.log(doc.id, " => ", doc.data());
+                vm.options_oil.push(doc.data())
+            });
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
     },
 })
