e57a2c6a4b8336e49f760cf9cf3f1a67753883d0
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-11 16:21:52 +0800

    Styling

--- calc.js
+++ calc.js
@@ -1,32 +1,37 @@
 new Vue({
     el: "#vue_component",
     data: {
-        options_oil: [],
-        selected_oil: null,
         oils_array: [],
     },
     created: function(){
         console.log('in created')
     },
     computed: {
-        useThisButtonDisabledText: function(){
-            if(this.selected_oil && this.selected_oil.name){
-                return false
-            } else {
-                return true
-            }
-        }
+        //useThisButtonDisabledText: function(){
+        //    if(this.selected_oil && this.selected_oil.name){
+        //        return false
+        //    } else {
+        //        return true
+        //    }
+        //}
     },
     methods: {
-        useThisOilClicked: function(){
-            if (!this.containsObject(this.selected_oil, this.oils_array)){
-                let oil_copied = JSON.parse(JSON.stringify(this.selected_oil))
-                oil_copied.quantity = 0
-                this.oils_array.push(oil_copied)
-            } else {
-                console.log('this oil is already added')
-            }
+        oilClicked: function(e, oil){
+            console.log('oilClicked')
+            console.log(oil)
+            //oil.selected = !oil.selected
+            Vue.set(oil, 'selected', !oil.selected)
+
         },
+        //useThisOilClicked: function(){
+        //    if (!this.containsObject(this.selected_oil, this.oils_array)){
+        //        let oil_copied = JSON.parse(JSON.stringify(this.selected_oil))
+        //        oil_copied.quantity = 0
+        //        this.oils_array.push(oil_copied)
+        //    } else {
+        //        console.log('this oil is already added')
+        //    }
+        //},
         containsObject: function(obj, arr) {
             var i;
             for (i = 0; i < arr.length; i++) {
@@ -36,27 +41,27 @@ new Vue({
             }
             return false;
         },
-        deleteThisOilClicked: function(e, item){
-            console.log('delete: ')
-            console.log(item)
-            this.oils_array.splice(this.oils_array.indexOf(item), 1 );
-        },
+        //deleteThisOilClicked: function(e, item){
+        //    console.log('delete: ')
+        //    console.log(item)
+        //    this.oils_array.splice(this.oils_array.indexOf(item), 1 );
+        //},
     },
     mounted: function(){
         console.log('in mounted')
         let vm = this
 
-        //axios.get("https://cocosavon.github.io/calc/my_data.json")
-        //    .then(response => {
-        //        vm.options_oil = response.oils
-        //    })
-
         let docRef = db.collection("oils")
         console.log(docRef)
         docRef.get().then(function(res) {
             res.forEach(doc => {
-                console.log(doc.id, " => ", doc.data());
-                vm.options_oil.push(doc.data())
+                let oil = doc.data()
+                oil.selected = false
+                oil.quantity = 0
+                oil.id = doc.id
+                vm.oils_array.push(oil)
+
+                console.log(oil)
             });
         }).catch(function(error) {
             console.log("Error getting document:", error);
